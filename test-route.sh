7efb4a7794b7583594dcfb5ab53d9ba03abe85d6
crigdon <craig.rigdon@gov.bc.ca>
2019-10-30 15:29:40 -0700

    adding test

---
+++ test-route.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# to be used to test hubot routes during development
+# you will have to change local ip and port to what you have defined in your docker port bindings for that container.
+
+curl -X POST -H "Content-Type: application/json" -d '{"status":"success", "stage":"test"}' http://127.0.0.1:32768/hubot/test
