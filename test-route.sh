2bb0c8ce66b7f98a72524128c05e5cd3c15d4956
crigdon <craig.rigdon@gov.bc.ca>
2019-11-28 16:46:52 -0800

    payload work

--- test-route.sh
+++ test-route.sh
@@ -6,9 +6,28 @@
 #curl -X POST -H "Content-Type: application/json" \
 #-d '{"status":"success", "stage":"test"}' http://127.0.0.1:32768/hubot/test
 
-curl -X POST -H "Content-Type: application/json" \
--d '{"status":"success or failed", "env":"cadi or cati here", "results":"test results here", "id":"12345"}' http://127.0.0.1:32768/hubot/apitest
+#curl -X POST -H "Content-Type: application/json" \
+#-d '{"status":"success or failed", "env":"cadi or cati here", "results":"test results here", "id":"12345"}' http://127.0.0.1:32768/hubot/apitest
 
 #curl -X POST -H "Content-Type: application/json" \
 #-d <payload-here> http://127.0.0.1:32768/hubot/github
 
+#PathTOJson='/Users/crigdon/PycharmProjects/pipeline-bot/data/build_deploy_response.json'
+##curl -X POST -H "Content-Type: application/json" -H "apikey: ${{ secrets.BOT_KEY }}" -d @$GITHUB_EVENT_PATH https://${{ secrets.BOT_URL }}/hubot/github
+#
+#MyString= | jq -Rs '.' '/Users/crigdon/PycharmProjects/pipeline-bot/data/build_deploy_response.json'
+##echo $MyString
+#
+#curl -d =$PAYLOAD -X POST -H "Content-Type: application/json" -H "apikey: ${{ secrets.BOT_KEY }}" https://${{ secrets.BOT_URL }}/hubot/github
+
+#working payload
+#Payload=$(cat '/Users/crigdon/PycharmProjects/pipeline-bot/data/build_deploy_response.json' | jq -r -S '. + {"envKey": "dev"}')
+#echo "hello"
+#echo $Payload
+
+#working payload
+payload=$(cat '/Users/crigdon/PycharmProjects/pipeline-bot/data/build_deploy_response.json')
+echo $payload
+echo $payload | jq --arg envKey dev '. + {envKey: $envKey}'
+
+curl -X POST -H "Content-Type: application/json"  -d "$payload" http://127.0.0.1:32768/hubot/github/dev
